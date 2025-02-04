a9836f6fe2d539787b113b1feb7f9eaf6ef4a1f2
mathys <lolmitoman@gmail.com>
2025-02-04 15:51:42 +0100

    ex3

---
+++ modules/app.js
@@ -0,0 +1,8 @@
+import { products } from "./products.js";
+import { buildProductsList } from "./ui.js";
+
+
+export function init(){
+    const productsList = document.getElementById('product-list');
+    productsList.innerHTML = buildProductsList(products);
+}
