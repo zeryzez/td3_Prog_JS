bfc8fcc431061ec0fe7f7dffa4d12b82ea9df28e
mathys <lolmitoman@gmail.com>
2025-02-04 17:15:25 +0100

    ex5

--- modules/app.js
+++ modules/app.js
@@ -1,8 +1,8 @@
-import { products } from "./products.js";
+import { productsFiltered } from "./products.js";
 import { buildProductsList } from "./ui.js";
 
 
 export function init(){
     const productsList = document.getElementById('product-list');
-    productsList.innerHTML = buildProductsList(products);
+    productsList.innerHTML = buildProductsList(productsFiltered);
 }
--- modules/products.js
+++ modules/products.js
@@ -8,8 +8,10 @@ let p1 = new Product('ref1', 100, 'desc1');
 let p2 = new Product('ref2', 200, 'desc2');
 let p3 = new Product('ref3', 300, 'desc3');
 
-export const products = [p1, p2, p3];
+const products = [p1, p2, p3];
+export let productsFiltered = products;
 
 export function search(keyword){
-    return products.filter(p => p.description.includes(keyword) || p.ref.includes(keyword));
+    productsFiltered =  products.filter(p => p.description.includes(keyword) || p.ref.includes(keyword));
+    return productsFiltered;
 }
