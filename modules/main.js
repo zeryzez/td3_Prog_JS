98b691a6717c9adaf066e24fc14a19a2686a2f6b
mathys <lolmitoman@gmail.com>
2025-02-04 15:52:36 +0100

    ex4

---
+++ modules/main.js
@@ -0,0 +1,10 @@
+import {init} from './app.js';
+import {search} from './products.js';
+
+const productList = document.getElementById('product-list');
+const research = document.getElementById('product-search');
+
+window.addEventListener('load', init);
+
+
+
